5b2d2d1a9502ed32c4705ab822eafc11f3f44230
Fir <834772509@qq.com>
2025-05-27 05:51:31 +0800

    update windows crate

--- build.rs
+++ build.rs
@@ -5,20 +5,4 @@ fn main() {
 
     // 内置资源
     embed_resource::compile("./resource/resource.rc", embed_resource::NONE);
-
-    // 声明绑定WinAPI
-    windows::build!(
-        Windows::Win32::Intl::GetUserDefaultUILanguage,
-
-        // Windows::Win32::SystemServices::{PWSTR, BOOL},
-        Windows::Win32::SystemServices::{NULL, INVALID_HANDLE_VALUE},
-        Windows::Win32::DeviceAndDriverInstallation::{UpdateDriverForPlugAndPlayDevicesW},
-        Windows::Win32::WindowsAndMessaging::HWND,
-
-        Windows::Win32::DeviceAndDriverInstallation::*,
-        Windows::Win32::Debug::GetLastError,
-
-       Windows::Win32::SystemServices::{PWSTR,HANDLE},
-       Windows::Win32::SystemServices::INVALID_HANDLE_VALUE,
-    );
 }
